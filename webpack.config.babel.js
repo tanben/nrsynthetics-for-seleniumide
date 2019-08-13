1428cf0c88a0ab7ae9a9f9b819a14801aef7208e
Benedicto Tan <btan@newrelic.com>
2019-08-13 18:07:37 -0400

    cleanup

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -48,12 +48,7 @@ module.exports = {
   module: {
     rules: [
       {
-        // "oneOf" will traverse all following loaders until one will
-        // match the requirements. When no loader matches it will fall
-        // back to the "file" loader at the end of the loader list.
         oneOf: [
-          // "url" loader works just like "file" loader but it also embeds
-          // assets smaller than specified size as data URLs to avoid requests.
           {
             test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
             loader: 'url-loader',
@@ -79,28 +74,13 @@ module.exports = {
             ]
           },
 
-          // Process markdown to string, to be included inside the bundle, instead of as file
-          {
-            test: /\.md$/,
-            loader: 'raw-loader',
-          },
-          // "file" loader makes sure assets end up in the `build` folder.
-          // When you `import` an asset, you get its filename.
-          // This loader don't uses a "test" so it will catch all modules
-          // that fall through the other loaders.
           {
             loader: 'file-loader',
-            // Exclude `js` files to keep "css" loader working as it injects
-            // it's runtime that would otherwise processed through "file" loader.
-            // Also exclude `html` and `json` extensions so they get processed
-            // by webpacks internal loaders.
             exclude: [/\.jsx?$/, /\.html$/, /\.json$/],
             options: {
               name: 'media/[name].[hash:8].[ext]',
             },
           },
-          // ** STOP ** Are you adding a new loader?
-          // Make sure to add the new loader(s) before the "file" loader.
         ],
       },
     ],
@@ -116,13 +96,9 @@ module.exports = {
         }),
 
 
-      // Copy non-umd assets to vendor
         new CopyWebpackPlugin([
           { from: "icons", to: "../icons" }
         ]),
-        // Makes some environment variables available to the JS code, for example:
-        // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
-        // It is absolutely essential that NODE_ENV was set to production here.
         new webpack.DefinePlugin({
           "process.env": {
             "NODE_ENV": JSON.stringify(process.env.NODE_ENV),
@@ -132,11 +108,6 @@ module.exports = {
           }
         }),
 
-        // Moment.js is an extremely popular library that bundles large locale files
-        // by default due to how Webpack interprets its code. This is a practical
-        // solution that requires the user to opt into importing specific locales.
-        // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
-        // You can remove this if you don't use Moment.js:
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
     ]
 
